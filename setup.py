bf7952d6a703367710ae578b449bc87ceb5f9bf6
Klaas Bosteels <klaas.bosteels@gmail.com>
2019-12-19 12:57:16 +0100

    Version bump and min dependency versions

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'crudest',
-    version = '0.1',
+    version = '0.2',
     license = 'Apache Software License (ASF)',
     packages = find_packages(),
-    install_requires = ['Flask', 'apispec', 'webargs', 'marshmallow',
-                        'flask-swagger-ui', 'Flask-JWT-Extended'],
+    install_requires = ['Flask', 'apispec>3.0', 'webargs', 'marshmallow>3.0',
+                        'flask-swagger-ui>3.0', 'Flask-JWT-Extended'],
     test_suite = 'nose.collector',
     tests_require = ['nose']
 )
