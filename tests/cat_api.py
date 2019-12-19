f7c33f7c6c25d7d95a12e4473357e71ac1dbe5d1
Klaas Bosteels <klaas.bosteels@gmail.com>
2019-12-19 12:45:53 +0100

    Introduce unit tests

---
+++ tests/cat_api.py
@@ -0,0 +1,184 @@
+from typing import collections
+
+from flask.app import Flask
+from flask.json import jsonify
+from marshmallow import Schema, fields
+
+from crudest import (CreateResource, CrudResource, DeleteResource, HeadedResponse, RestApi,
+                     RetrieveResource, UpdateResource, WrappedResponse, extra_args, jwt_required)
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 's3cr1t'
+
+api = RestApi(app, title='Cat API', version='v1')
+
+db = collections.defaultdict(dict)
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['msg'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+class IdSchema(Schema):
+    id = fields.Int()
+
+
+class CatSchema(IdSchema):
+    name = fields.Str()
+    weight = fields.Decimal(as_string=True)
+
+
+class CatWhiskerSchema(IdSchema):
+    cat_id = fields.Int()
+    length = fields.Decimal(as_string=True)
+
+
+class CatActionSchema(IdSchema):
+    verb = fields.Str()
+
+
+@api.resource('/cats/<int:cat_id>', name='Cat', schema=CatSchema)
+class CatResource(CreateResource, RetrieveResource, UpdateResource, DeleteResource):
+
+    @jwt_required
+    def create(self, **kwargs):
+        cat_id = max(db['Cat'].keys() or [0]) + 1
+        cat = {'id': cat_id, **kwargs}
+        db['Cat'][cat_id] = cat
+        return cat
+
+    @jwt_required
+    @extra_args({'page': fields.Int(missing=1)})
+    def list(self, page):
+        links = {}
+        values = list(db['Cat'].values())
+        if (page + 1) * 2 <= (len(values) // 2 + 1) * 2:  # is there another page?
+            links['next'] = api.url_for('Cat', page=page+1)
+        # grabbing this opportunity to test headed responses!
+        return HeadedResponse(
+            data=values[(page-1)*2:page*2],
+            links=links
+        )
+
+    @jwt_required
+    def retrieve(self, cat_id):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        return db['Cat'][cat_id]
+
+    @jwt_required
+    def update(self, cat_id, **kwargs):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        updated_cat = {**db['Cat'][cat_id], **kwargs}
+        db['Cat'][cat_id] = updated_cat
+        return updated_cat
+
+    @jwt_required
+    def delete(self, cat_id):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        del db['Cat'][cat_id]
+        # delete corresponding whiskers as well
+        for cat_whisker in list(db['CatWhisker'].values()):
+            if cat_whisker['cat_id'] == cat_id:
+                del db['CatWhisker'][cat_whisker['id']]
+
+
+@api.resource('/cats/<int:cat_id>/whiskers/<int:cat_whisker_id>', name='CatWhisker', schema=CatWhiskerSchema)
+class CatWhiskerResource(CrudResource):
+
+    @jwt_required
+    def create(self, cat_id, **kwargs):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        cat_whisker_id = max(db['CatWhisker'].keys() or [0]) + 1
+        cat_whisker = {'id': cat_whisker_id, 'cat_id': cat_id, **kwargs}
+        db['CatWhisker'][cat_whisker_id] = cat_whisker
+        return cat_whisker
+
+    @jwt_required
+    @extra_args({'page': fields.Int(missing=1)})
+    def list(self, cat_id, page):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        links = {}
+        values = [
+            value for value in db['CatWhisker'].values()
+            if value['cat_id'] == cat_id
+        ]
+        if (page + 1) * 3 <= (len(values) // 3 + 1) * 3:  # is there another page?
+            links['next'] = api.url_for('CatWhisker', cat_id=cat_id, page=page+1)
+        # grabbing this opportunity to test wrapped responses!
+        return WrappedResponse(
+            data=values[(page-1)*3:page*3],
+            links=links
+        )
+
+    @jwt_required
+    def retrieve(self, cat_id, cat_whisker_id):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        if cat_whisker_id not in db['CatWhisker']:
+            raise InvalidUsage('Whisker not found.', status_code=404)
+        return db['CatWhisker'][cat_whisker_id]
+
+    @jwt_required
+    def update(self, cat_id, cat_whisker_id, **kwargs):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        if cat_whisker_id not in db['CatWhisker']:
+            raise InvalidUsage('Whisker not found.', status_code=404)
+        updated_cat_whisker = {**db['CatWhisker'][cat_whisker_id], **kwargs}
+        db['CatWhisker'][cat_whisker_id] = updated_cat_whisker
+        return updated_cat_whisker
+
+    @jwt_required
+    def delete(self, cat_id, cat_whisker_id):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        if cat_whisker_id not in db['CatWhisker']:
+            raise InvalidUsage('Whisker not found.', status_code=404)
+        del db['CatWhisker'][cat_whisker_id]
+
+
+@api.resource('/cats/<int:cat_id>/actions/<int:cat_action_id>', name='CatAction', schema=CatActionSchema)
+class CatActionResource(CreateResource, UpdateResource):
+
+    @jwt_required
+    def create(self, cat_id, verb):
+        return self.update(cat_id, 1, verb)
+
+    @jwt_required
+    def update(self, cat_id, cat_action_id, verb):
+        if cat_id not in db['Cat']:
+            raise InvalidUsage('Cat not found.', status_code=404)
+        cat_name = db['Cat'][cat_id]['name']
+        return WrappedResponse(
+            data={
+                'id': 1,
+                'result': f'The cat named {cat_name} {verb}ed.'
+            },
+            links=dict(
+                self=api.url_for('CatAction', cat_id=cat_id, cat_action_id=1)
+            )
+        )
---
+++ tests/test_cats_api.py
@@ -0,0 +1,222 @@
+from decimal import Decimal
+from requests_flask_adapter import Session
+
+import pytest
+
+from crudest import create_access_token, create_refresh_token
+
+from .cat_api import app, db
+
+
+@pytest.fixture
+def client():
+    # fancier alternative for app.test_client() that uses requests
+    app.config['SERVER_NAME'] = 'feline.io'
+    Session.register('http://feline.io', app)
+    return Session()
+
+
+@pytest.fixture
+def access_token():
+    with app.app_context():
+        yield create_access_token(identity=1, fresh=True)
+
+
+@pytest.fixture
+def refresh_token():
+    with app.app_context():
+        yield create_refresh_token(identity=1)
+
+
+@pytest.fixture
+def database():
+    db.clear()
+    db['User'][1] = {'id': 1, 'name': 'Jon Arbuckle'}
+    db['Cat'][1] = {'id': 1, 'name': 'Garfield', 'weight': Decimal('24.67')}
+    db['Cat'][2] = {'id': 2, 'name': 'Leftfield', 'weight': Decimal('23.28')}
+    db['Cat'][3] = {'id': 3, 'name': 'Rightfield', 'weight': Decimal('22.73')}
+    db['CatWhisker'][1] = {'id': 1, 'cat_id': 1, 'length': Decimal('10.57')}
+    db['CatWhisker'][2] = {'id': 2, 'cat_id': 1, 'length': Decimal('11.03')}
+    db['CatWhisker'][3] = {'id': 3, 'cat_id': 1, 'length': Decimal('9.95')}
+    db['CatWhisker'][4] = {'id': 4, 'cat_id': 1, 'length': Decimal('10.34')}
+    return db
+
+
+def test_create_cat(client, access_token, database):
+    name = 'Simba'
+    weight = Decimal('12.34')
+
+    rv = client.post('http://feline.io/cats', headers={
+        'Authorization': 'Bearer ' + access_token
+    }, data={
+        'name': name,
+        'weight': weight
+    })
+
+    assert rv.status_code == 201
+    stored = database['Cat'][rv.json()['id']]
+    assert stored['name'] == name
+    assert stored['weight'] == weight
+
+
+def test_create_cat_whisker(client, access_token, database):
+    cat_id = 1
+    length = Decimal('12.34')
+
+    rv = client.post(f'http://feline.io/cats/{cat_id}/whiskers', headers={
+        'Authorization': 'Bearer ' + access_token
+    }, data={
+        'length': length
+    })
+
+    assert rv.status_code == 201
+    stored = database['CatWhisker'][rv.json()['id']]
+    assert stored['cat_id'] == cat_id
+    assert stored['length'] == length
+
+
+def test_create_cat_action(client, access_token, database):
+    cat_id = 1
+    verb = 'meow'
+
+    rv = client.post(f'http://feline.io/cats/{cat_id}/actions', headers={
+        'Authorization': 'Bearer ' + access_token
+    }, data={
+        'verb': verb
+    })
+
+    assert rv.status_code == 201
+    assert rv.json()['links']['collection'] == 'http://feline.io/cats/1/actions'
+
+
+def test_list_cats(client, access_token, database):
+    rv = client.get('http://feline.io/cats', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+    results = rv.json()
+
+    assert rv.status_code == 200
+    assert isinstance(results, list)
+    assert len(results) == 2  # page size is 2
+
+    first_result = results[0]
+    stored = database['Cat'][1]
+    for key in stored.keys():
+        assert str(first_result[key]) == str(stored[key])  # to str because decimals
+
+    assert 'link' in rv.headers
+    assert rv.links['self']['url'] == 'http://feline.io/cats'
+    assert rv.links['next']['url'] == 'http://feline.io/cats?page=2'
+
+    rv = client.get(f'http://feline.io/cats?page=2', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+    results = rv.json()
+
+    assert rv.status_code == 200
+    assert len(results) == 1  # only 1 left on last page
+    assert 'next' not in rv.links
+
+
+def test_list_cat_whiskers(client, access_token, database):
+    rv = client.get('http://feline.io/cats/1/whiskers', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+    results = rv.json()
+
+    assert rv.status_code == 200
+    assert isinstance(results, dict)
+    assert 'data' in results
+    assert len(results['data']) == 3  # page size is 3
+
+    first_result = results['data'][0]
+    stored = database['CatWhisker'][1]
+    for key in stored.keys():
+        assert str(first_result[key]) == str(stored[key])  # to str because decimals
+
+    assert 'links' in results
+    assert results['links']['self'] == 'http://feline.io/cats/1/whiskers'
+    assert results['links']['next'] == 'http://feline.io/cats/1/whiskers?page=2'
+
+    rv = client.get(f'http://feline.io/cats/1/whiskers?page=2', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+    results = rv.json()
+
+    assert rv.status_code == 200
+    assert len(results['data']) == 1  # only 1 left on last page
+    assert 'next' not in results['links']
+
+
+def test_retrieve_cat(client, access_token, database):
+    rv = client.get('http://feline.io/cats/1', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+    result = rv.json()
+
+    assert rv.status_code == 200
+    stored = database['Cat'][1]
+    for key in stored.keys():
+        assert str(result[key]) == str(stored[key])  # to str because decimals
+
+
+def test_retrieve_cat_whisker(client, access_token, database):
+    rv = client.get('http://feline.io/cats/1/whiskers/1', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+    result = rv.json()
+
+    assert rv.status_code == 200
+    stored = database['CatWhisker'][1]
+    for key in stored.keys():
+        assert str(result[key]) == str(stored[key])  # to str because decimals
+
+
+def test_update_cat(client, access_token, database):
+    name = 'Garfield aka The Fat Cat'
+
+    rv = client.put('http://feline.io/cats/1', headers={
+        'Authorization': 'Bearer ' + access_token
+    }, data={
+        'name': name
+    })
+    result = rv.json()
+
+    assert rv.status_code == 200
+    stored = database['Cat'][result['id']]
+    assert stored['name'] == name
+    assert stored['weight'] == Decimal(result['weight'])
+
+
+def test_update_cat_whisker(client, access_token, database):
+    length = Decimal('9.99')
+
+    rv = client.put('http://feline.io/cats/1/whiskers/1', headers={
+        'Authorization': 'Bearer ' + access_token
+    }, data={
+        'length': length
+    })
+    result = rv.json()
+
+    assert rv.status_code == 200
+    stored = database['CatWhisker'][result['id']]
+    assert stored['length'] == Decimal(result['length'])
+
+
+def test_delete_cat(client, access_token, database):
+    rv = client.delete('http://feline.io/cats/1', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+
+    assert rv.status_code == 204
+    assert 1 not in database['Cat']
+    assert 1 not in set(cw['cat_id'] for cw in database['CatWhisker'].values())
+
+
+def test_delete_cat_whisker(client, access_token, database):
+    rv = client.delete('http://feline.io/cats/1/whiskers/1', headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+
+    assert rv.status_code == 204
+    assert 1 not in database['CatWhisker']
